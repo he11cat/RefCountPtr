105b2b98ad15c98fed19eb44e0078cca0080a88d
Pacino Li <leli@zynga.com>
2014-04-01 19:28:33 +0800

    initial implementation

---
+++ RefCountPtr.cpp
@@ -0,0 +1,54 @@
+#include "RefCountPtr.h"
+
+RefCountable::RefCountable() : refCount(0)
+{
+    cout << "Ctor..." << hex << this << endl;
+}
+
+RefCountable::~RefCountable()
+{
+
+}
+
+int RefCountable::addRef()
+{
+    // guard.lock();
+    ++refCount;
+    // guard.unlock();
+    return refCount;
+}
+
+int RefCountable::removeRef()
+{
+    // guard.lock();
+    --refCount;
+    // guard.unlock();
+    return refCount;
+}
+
+My::~My()
+{
+    release();
+}
+
+void My::release()
+{
+    cout << "release..." << hex << this << endl;
+}
+
+int main()
+{
+    RefCountPtr<My> p1 = new My();
+
+    RefCountPtr<My> p2 = new My();
+
+    p2 = p1;
+
+    My m;
+
+    My* p = &p1;
+
+    My& pr = *p2;
+
+    return 0;
+}
---
+++ RefCountPtr.h
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <atomic>
+#include <mutex>
+using namespace std;
+
+class RefCountable
+{
+    public:
+        RefCountable();
+        virtual ~RefCountable();
+
+        int addRef();
+        int removeRef();
+        virtual void release() = 0;
+
+    private:
+        volatile int refCount;
+        //field 'guard' has an inaccessible copy constructor
+        //mutex guard;
+};
+
+class My : public RefCountable
+{
+    public:
+        virtual ~My();
+        virtual void release();
+};
+
+template<class T>
+class RefCountPtr
+{
+    public:
+        RefCountPtr(): ptr(NULL) {}
+
+        RefCountPtr(T* p)
+        {
+            ptr = p;
+            ptr->addRef();
+        }
+
+        RefCountPtr(const RefCountPtr& another)
+        {
+            ptr = another.ptr;
+            ptr->addRef();
+        }
+
+        RefCountPtr& operator = (const RefCountPtr& another)
+        {
+            if (ptr != NULL && ptr->removeRef() == 0)
+            {
+                ptr->release();
+                ptr = NULL;
+            }
+
+            ptr = another.ptr;
+            ptr->addRef();
+            return *this;
+        }
+
+        ~RefCountPtr()
+        {
+            if (ptr->removeRef() == 0)
+            {
+                ptr->release();
+                ptr = NULL;
+            }
+        }
+
+        T* operator&()
+        {
+            return ptr;
+        }
+
+        T& operator*()
+        {
+            return *ptr;
+        }
+
+    private:
+        T* ptr;
+};
